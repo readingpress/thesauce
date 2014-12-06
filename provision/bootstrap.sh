04dc5c6344f3e5832d1399ffb83880403873590e
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-06 18:32:07 -0500

    Update bootstrap to upload local php config files.

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -5,14 +5,10 @@ apt-get update
 # require user input.
 export DEBIAN_FRONTEND=noninteractive
 apt-get install mysql-server nginx php5-fpm -y
-# This protects against php execution of files with other extensions that could
-# be uploaded by a user.
-# @see https://nealpoole.com/blog/2011/04/setting-up-php-fastcgi-and-nginx-dont-trust-the-tutorials-check-your-configuration/
-sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
-# Upload vhosts config to nginx and enable the site. In nginx this is 
-# accomplished by creating a symlink from a sites-available config file to 
-#	sites-enabled.
+cp /vagrant/provision/config/php.ini /etc/php5/fpm/php.ini
+cp /vagrant/provision/config/www.conf /etc/php5/fpm/pool.d/www.conf
 cp /vagrant/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost
 ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/nginx_vhost
 rm /etc/nginx/sites-enabled/default
-service nginx start
+sudo service php5-fpm restart
+service nginx restart
