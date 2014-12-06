34dc7b672f0ee57866bf928f231c74331aa3398c
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-06 17:51:16 -0500

    Configure php.

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -4,7 +4,11 @@ apt-get update
 # We set DEBIAN_FRONTEND to noninteractive in order to bypass all prompts that
 # require user input.
 export DEBIAN_FRONTEND=noninteractive
-apt-get -y install mysql-server nginx php5-fpm -y
+apt-get install mysql-server nginx php5-fpm -y
+# This protects against php execution of files with other extensions that could
+# be uploaded by a user.
+# @see https://nealpoole.com/blog/2011/04/setting-up-php-fastcgi-and-nginx-dont-trust-the-tutorials-check-your-configuration/
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
 # Upload vhosts config to nginx and enable the site. In nginx this is 
 # accomplished by creating a symlink from a sites-available config file to 
 #	sites-enabled.
