7762f416757505909f22a714019d3a12df14779e
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-06 19:51:46 -0500

    Remove debug code in index.php.

--- docroot/index.php
+++ docroot/index.php
@@ -1,2 +1,10 @@
-<?php
-phpinfo();
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>The Sauce</title>
+	</head>
+	<body>
+		<h1>Welcome to the sauce</h1>
+		<p>The sauce is a fully configured vagrant-based LEMP (Linux, nginx, MySQL, PHP) stack ready for your use.</p>
+	</body>
+</html>
