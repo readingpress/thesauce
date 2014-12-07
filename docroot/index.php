08b5f1a0645a019247c1b0ad1fa776869dd716a4
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-07 01:04:34 -0500

    Add comment

--- docroot/index.php
+++ docroot/index.php
@@ -2,13 +2,12 @@
 /**
  *	@file
  *	Handles all incoming http requests that are not for a file in a public 
- *	directory.
+ *	directory or a public directory itself..
  */
-
 
 /**
  * Root directory of the application.
  */
 define('APP_ROOT', getcwd());
-
+// Let's make the magic happen.
 require_once APP_ROOT . '/application/bootstrap.php';
