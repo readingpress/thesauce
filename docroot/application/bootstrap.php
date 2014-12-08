f427f0dffae3d9a004cc797a676552833b1a09fb
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-08 00:41:52 -0500

    Fix final bugs during tests.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -6,6 +6,7 @@
 
 // Load dependencies.
 require_once APP_ROOT . '/application/model/sql/query.inc';
-require_once APP_ROOT . '/application/model/sql/db_admin.inc';
 require_once APP_ROOT . '/application/model/sql/insert_query.inc';
 require_once APP_ROOT . '/application/model/sql/select_query.inc';
+require_once APP_ROOT . '/application/model/sql/update_query.inc';
+require_once APP_ROOT . '/application/model/sql/delete_query.inc';
