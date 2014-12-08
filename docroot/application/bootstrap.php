3e509d516e60accc5dd3b44c8f621d06e49d0e02
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-07 23:37:58 -0500

    Stub out insert query class.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -5,6 +5,7 @@
  */
 
 // Load dependencies.
-require_once APP_ROOT . '/application/model/sql/query.php';
-require_once APP_ROOT . '/application/model/sql/db_admin.php';
-require_once APP_ROOT . '/application/model/sql/select_query.php';
+require_once APP_ROOT . '/application/model/sql/query.inc';
+require_once APP_ROOT . '/application/model/sql/db_admin.inc';
+require_once APP_ROOT . '/application/model/sql/insert_query.inc';
+require_once APP_ROOT . '/application/model/sql/select_query.inc';
