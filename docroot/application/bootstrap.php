6429258a39a69bf55cf1ab31df850687daa51089
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-12 00:14:54 -0500

    Remove property and method name collision.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -10,14 +10,3 @@ require_once APP_ROOT . '/application/model/sql/insert_query.inc';
 require_once APP_ROOT . '/application/model/sql/select_query.inc';
 require_once APP_ROOT . '/application/model/sql/update_query.inc';
 require_once APP_ROOT . '/application/model/sql/delete_query.inc';
-
-$a = new InsertQuery('Persons');
-$a->values(array(
-	'ID' => 11,
-	'FirstName' => 'Walter',
-	'LastName' => 'White',
-	'Address' => 'Albuquerque',
-	'City' => 'New Mexico',
-));
-
-var_dump($a->toString());
