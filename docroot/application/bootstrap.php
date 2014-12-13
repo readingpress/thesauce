a812f9628d791fb73476bdd4a45475ef61954d87
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-13 14:03:14 -0500

    Add type validation to all code.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -5,8 +5,11 @@
  */
 
 // Load dependencies.
+require_once APP_ROOT . '/application/model/type/type_validator.inc';
 require_once APP_ROOT . '/application/model/sql/query.inc';
 require_once APP_ROOT . '/application/model/sql/insert_query.inc';
 require_once APP_ROOT . '/application/model/sql/select_query.inc';
 require_once APP_ROOT . '/application/model/sql/update_query.inc';
 require_once APP_ROOT . '/application/model/sql/delete_query.inc';
+
+TypeValidator::strings('POW', 'sista', 'brothas');
