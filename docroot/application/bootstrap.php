fa605c6e007c009f5864c8415e633a736ac1ed79
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-13 17:56:30 -0500

    Remove error reporting.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -5,10 +5,8 @@
  */
 
 // Load dependencies.
-require_once APP_ROOT . '/application/error/error.php';
 require_once APP_ROOT . '/application/model/sql/query.inc';
 require_once APP_ROOT . '/application/model/sql/insert_query.inc';
 require_once APP_ROOT . '/application/model/sql/select_query.inc';
 require_once APP_ROOT . '/application/model/sql/update_query.inc';
 require_once APP_ROOT . '/application/model/sql/delete_query.inc';
-set_error_handler ('catch_php_errors');
