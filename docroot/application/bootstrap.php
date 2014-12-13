e1b1d551aa006c938c49aa6b2f67afe79953bd11
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-13 14:48:02 -0500

    Refactor type validation.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -5,11 +5,8 @@
  */
 
 // Load dependencies.
-require_once APP_ROOT . '/application/model/type/type_validator.inc';
 require_once APP_ROOT . '/application/model/sql/query.inc';
 require_once APP_ROOT . '/application/model/sql/insert_query.inc';
 require_once APP_ROOT . '/application/model/sql/select_query.inc';
 require_once APP_ROOT . '/application/model/sql/update_query.inc';
 require_once APP_ROOT . '/application/model/sql/delete_query.inc';
-
-TypeValidator::strings('POW', 'sista', 'brothas');
