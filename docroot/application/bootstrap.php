93cf9738366968428ae3302d00c982d4f61b30a1
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-14 16:47:39 -0500

    Add a query interface.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -5,6 +5,7 @@
  */
 
 // Load dependencies.
+require_once APP_ROOT . '/application/model/sql/query_interface.inc';
 require_once APP_ROOT . '/application/model/sql/query.inc';
 require_once APP_ROOT . '/application/model/sql/insert_query.inc';
 require_once APP_ROOT . '/application/model/sql/select_query.inc';
