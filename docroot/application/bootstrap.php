17eab423d7ea0dd9c6fb4fb1ec50d7df42909096
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-14 16:54:57 -0500

    Add a queryfactory class.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -11,3 +11,4 @@ require_once APP_ROOT . '/application/model/sql/insert_query.inc';
 require_once APP_ROOT . '/application/model/sql/select_query.inc';
 require_once APP_ROOT . '/application/model/sql/update_query.inc';
 require_once APP_ROOT . '/application/model/sql/delete_query.inc';
+require_once APP_ROOT . '/application/model/sql/query_factory.inc';
