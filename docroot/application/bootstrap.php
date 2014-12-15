fe41d6a891ca8f5fe9b3047f5275a67d04eb0116
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-14 19:07:10 -0500

    Add Drop and create query support.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -7,14 +7,15 @@
 // Load dependencies.
 require_once APP_ROOT . '/application/model/sql/query_interface.inc';
 require_once APP_ROOT . '/application/model/sql/query.inc';
+require_once APP_ROOT . '/application/model/sql/create_query.inc';
+require_once APP_ROOT . '/application/model/sql/drop_query.inc';
 require_once APP_ROOT . '/application/model/sql/insert_query.inc';
 require_once APP_ROOT . '/application/model/sql/select_query.inc';
 require_once APP_ROOT . '/application/model/sql/update_query.inc';
 require_once APP_ROOT . '/application/model/sql/delete_query.inc';
 require_once APP_ROOT . '/application/model/sql/query_factory.inc';
 
-$a = QueryFactory::createQuery('select', 'Persons', 'p')
-			->condition('p.LastName', 'DeWolf', '=')
-			->fields('p', array('FirstName', 'LastName'));
-var_dump($a->getStatement());
-var_dump($a->getParams());
+// @TODO
+//  Add alias regex input validation to params which require an alias prefix.
+//  Fix condition which breaks with more than one condition. Allow both AND and OR.
+//  Devise a way to include CREATE/DROP DATABASE and CREATE/DROP TABLE.
