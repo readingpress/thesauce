9153c1fbec559ba59d63933541dad4f19a13a3b5
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-14 21:25:30 -0500

    Add querymaking capacity.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -5,6 +5,7 @@
  */
 
 // Load dependencies.
+require_once APP_ROOT . '/application/model/sql/database_connection.inc';
 require_once APP_ROOT . '/application/model/sql/query_interface.inc';
 require_once APP_ROOT . '/application/model/sql/query.inc';
 require_once APP_ROOT . '/application/model/sql/create_query.inc';
@@ -19,3 +20,8 @@ require_once APP_ROOT . '/application/model/sql/query_factory.inc';
 //  Add alias regex input validation to params which require an alias prefix.
 //  Fix condition which breaks with more than one condition. Allow both AND and OR.
 //  Devise a way to include CREATE/DROP DATABASE and CREATE/DROP TABLE.
+
+$a = QueryFactory::createQuery('select', 'Persons', 'p')
+			->fields('p')
+			->condition('p.LastName', 'Curlin', '=')
+			->execute();
