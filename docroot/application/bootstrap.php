64be76dd975caa97eb76e2556eae013823c5f7ee
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-14 23:03:55 -0500

    Add more TODOs.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -20,6 +20,8 @@ require_once APP_ROOT . '/application/model/sql/query_factory.inc';
 //  Add alias regex input validation to params which require an alias prefix.
 //  Fix condition which breaks with more than one condition. Allow both AND and OR.
 //  Check to see if LIMIT and ORDER BY should be using palceholders.
+//  Look into Alias mess and see if there is a better way then including it in the
+//  interface constructor only so select queries can use it.
 //	Namespace and code polish entire SQL model according to best practices.
 //	Full code review.
 //
