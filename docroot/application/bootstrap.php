6c6caa64d52159c54e4ec2d2e707646349d77eb5
michaeldewolf85 <michaeldewolf85@gmail.com>
2014-12-16 17:18:02 -0500

    Add CRUD interface.

--- docroot/application/bootstrap.php
+++ docroot/application/bootstrap.php
@@ -5,6 +5,8 @@
  */
 
 // Load dependencies.
+//
+// SQL model
 require_once APP_ROOT . '/application/model/sql/database_connection.inc';
 require_once APP_ROOT . '/application/model/sql/query_interface.inc';
 require_once APP_ROOT . '/application/model/sql/database_query.inc';
@@ -15,20 +17,24 @@ require_once APP_ROOT . '/application/model/sql/select_query.inc';
 require_once APP_ROOT . '/application/model/sql/update_query.inc';
 require_once APP_ROOT . '/application/model/sql/delete_query.inc';
 require_once APP_ROOT . '/application/model/sql/query_factory.inc';
+// Idea model
+require_once APP_ROOT . '/application/model/idea/crud_interface.inc';
 
-// @TODO SQL Model
-//  Add alias regex input validation to params which require an alias prefix.
-//  Fix condition which breaks with more than one condition. Allow both AND and OR.
-//  Check to see if LIMIT and ORDER BY should be using palceholders.
-//  Look into Alias mess and see if there is a better way then including it in the
-//  interface constructor only so select queries can use it.
-//	Namespace and code polish entire SQL model according to best practices.
-//  Review afresh whether we are missing any validation functions or have
-//  unneccesary ones.
-//  Change filenames, classnames, and/or var names in accordance with best practices.
-//	Full code review, comment cleanup etc.
-//  Q/A all factory methods.
-//
-// @TODO Other
-//	Fix server config to use sed on www.conf and php.ini rather than overwriting the files
+// @TODO Server
+//	1) Fix server config to use sed on www.conf and php.ini rather than overwriting the files
 //	for forward compatibility with updates.
+// 
+// @TODO SQL Model
+//  1) Add alias regex input validation to params which require an alias prefix.
+//  2) Fix condition which breaks with more than one condition. Allow both AND and OR.
+//  3) Check to see if LIMIT and ORDER BY should be using placeholders.
+//  3) Look into Alias mess and see if there is a better way then including it in the
+//  interface constructor only so select queries can use it.
+//	4) Namespace and code polish entire SQL model according to best practices.
+//  5) Review afresh whether we are missing any validation functions or have
+//  unneccesary ones.
+//  6) Change filenames, classnames, and/or var names in accordance with best practices.
+//	7) Full code review, comment cleanup etc.
+//  8) Q/A all factory methods.
+//	9) Look into the difficulty and complexity introduced by including some of the
+//	the missing SQL methods and/or comparisons
